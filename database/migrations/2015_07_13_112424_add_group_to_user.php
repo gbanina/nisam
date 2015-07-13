bfee2dbc93d58aee7a283d09cdcfd89ebe0d2a50
Goran Banina <gbanina@gmail.com>
2015-07-13 14:33:17 +0200

    Groups migration.

---
+++ database/migrations/2015_07_13_112424_add_group_to_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGroupToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table){
+            $table->integer('group_id');
+            $table->foreign('group_id')->references('id')->on('group')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
