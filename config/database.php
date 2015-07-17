667389e8d485ff7a90a0c463d80a0e55cdaf2883
gbanina <gbanina@gmail.com>
2015-07-17 04:57:32 -0400

    Synchronising with live server.

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'root'),
+            'database'  => env('DB_DATABASE', 'nisam'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
